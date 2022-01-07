2c3ae71e96a5d2d0055d441e454bb1bea18713cd
Kameli71 <karim.kameli71@gmail.com>
2022-01-07 10:52:02 +0100

    syntax fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome team git workflow!</h1>
-      <h2>Coucou Louis !
-
-      </h2>
+      <h2>Coucou Louis !</h2>
     </div>
   );
 }
